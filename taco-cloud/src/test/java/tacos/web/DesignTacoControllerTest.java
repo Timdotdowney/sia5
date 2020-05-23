c02b89429d76bb502e133270b601ddf0d02cfa4a
Tim Downey <downeyt@cs.fiu.edu>
2020-05-22 21:54:21 -0400

    added test for submitting the design and saving the order. It must be done in the same test, as the test class is recreated after each test. The session attrubite must be added to the session before the perform.

--- taco-cloud/src/test/java/tacos/web/DesignTacoControllerTest.java
+++ taco-cloud/src/test/java/tacos/web/DesignTacoControllerTest.java
@@ -1,15 +1,18 @@
 package tacos.web;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.param;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -20,12 +23,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Order;
 import tacos.Taco;
+import tacos.data.OrderRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Slf4j
 class DesignTacoControllerTest {
 	
 	Taco taco1;
@@ -61,29 +67,76 @@ class DesignTacoControllerTest {
 		requestParams.add("ccCVV", "123");
 		
 	}
+	
+	@Autowired
+	private OrderRepository repoOrder;
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
+	void injectedComponentsAreNotNull() {
+		assertNotNull(repoOrder);
+	}
+	
+	@Test
 	public void testHomePage() throws Exception {
 		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home"))
 				.andExpect(content().string(containsString("Welcome to...")));
 	}
-	
-	@Test
-	public void testProcessDesign() throws Exception {
-		mockMvc.perform(post("/design").param("name","test1").param("ingredients", "FLTO"))
-			.andExpect(redirectedUrl("/orders/current"));
-	}
-	
+
 	@Test
 	public void testShowDesign() throws Exception {
-		mockMvc.perform(get("/design")).andExpect(status().isOk())
+		mockMvc.perform(get("/design"))
+			.andExpect(status().isOk())
 			.andExpect(view().name("design"))
 			.andExpect(content().string(containsString("Design your taco!")));
 	}
+
 	
+	@Test
+	public void testProcessDesign() throws Exception {
+		mockMvc.perform(
+				post("/design").sessionAttr("order", order1).param("name", "test1").param("ingredients", "FLTO"))
+				.andExpect(redirectedUrl("/orders/current"));
+	}
+
+	@Test
+	public void testProcessDesignGet() throws Exception {
+		mockMvc.perform(get("/orders/current").requestAttr("design", taco1).sessionAttr("order", order1))
+				.andExpect(status().isOk()).andExpect(view().name("orderForm"))
+				.andExpect(content().string(containsString("Order your taco creations!")));
+	}
+	
+	@Test
+	public void testProcessDesignPost() throws Exception {
+		mockMvc.perform(
+				post("/design")
+				.sessionAttr("order", order1)
+				.param("name", "test1")
+				.param("ingredients", "FLTO"))
+				.andExpect(redirectedUrl("/orders/current"));
+
+		mockMvc.perform(post("/orders")
+				.requestAttr("design", taco1)
+				.sessionAttr("order", order1)
+				.params(requestParams))
+				.andExpect(redirectedUrl("/"));
+
+		Iterable<Order> orders = repoOrder.findAll();
+		assertNotNull(orders);
+		Iterator<Order> it = orders.iterator();
+		assertTrue(it.hasNext());
+		Order orderNext = it.next();
+		assertEquals(orderNext.getId(), order1.getId());
+		assertNotNull(orderNext.getTacos());
+		Iterator<Taco> itTaco = orderNext.getTacos().iterator();
+		assertTrue(itTaco.hasNext());
+		Taco tacoNext = itTaco.next();
+		assertEquals(tacoNext.getId(), taco1.getId());
+		log.info("processed design");
+	}
+
 	@Test
 	public void testProcessOrder() throws Exception {
 		mockMvc.perform(post("/orders")
