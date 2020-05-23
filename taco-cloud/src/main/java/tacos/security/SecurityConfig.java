8334f3568e451f42d29e026860f636fcf645366e
Tim Downey <downeyt@cs.fiu.edu>
2020-05-23 13:16:55 -0400

    added formLogin code to config file for custom login page

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -14,9 +14,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	http
 		.authorizeRequests()
-		.antMatchers("/design", "/orders")
-		.access("hasRole('ROLE_USER')")
-		.antMatchers("/", "/**").access("permitAll")
-	;
+			.antMatchers("/design", "/orders")
+			.access("hasRole('ROLE_USER')")
+			.antMatchers("/", "/**").access("permitAll")
+		.and()
+			.formLogin()
+				.loginPage("/login");
 	}
 }
