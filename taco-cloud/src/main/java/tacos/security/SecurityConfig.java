44be69f25ebe9ae9613c4e5a48bc8787f1521b06
Tim Downey <downeyt@cs.fiu.edu>
2020-05-23 13:30:16 -0400

    configured a login page and limited access only to design and order paths. It all works.

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,24 +1,26 @@
 package tacos.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-	http
-		.authorizeRequests()
-			.antMatchers("/design", "/orders")
-			.access("hasRole('ROLE_USER')")
-			.antMatchers("/", "/**").access("permitAll")
-		.and()
-			.formLogin()
-				.loginPage("/login");
+		http.authorizeRequests().antMatchers("/design", "/orders").access("hasRole('ROLE_USER')")
+				.antMatchers("/", "/**").access("permitAll").and().formLogin().loginPage("/login");
 	}
 }
